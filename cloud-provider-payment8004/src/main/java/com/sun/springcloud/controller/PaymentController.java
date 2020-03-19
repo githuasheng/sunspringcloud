e52ab0ba65b1adf26560e07da2889194ae2e71a8
huasheng <279120247@qq.com>
2020-03-19 23:54:38 +0800

    提交DiscoveryClient注解功能

---
+++ cloud-provider-payment8004/src/main/java/com/sun/springcloud/controller/PaymentController.java
@@ -0,0 +1,16 @@
+package com.sun.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PaymentController {
+    @Value("${server.port}")
+    private String serverPort;
+
+    @RequestMapping(value = "/payment/zk")
+    public String paymentzk(){
+        return "springcloud with zookeeper"+serverPort+"\t";
+    }
+}
