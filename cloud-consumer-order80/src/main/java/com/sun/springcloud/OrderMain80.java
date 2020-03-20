d8a0af7c964e2f541a52c8578d03ab057d6fa392
huasheng <279120247@qq.com>
2020-03-20 22:59:17 +0800

    Ribbon的自定义和指定负载均衡设置

--- cloud-consumer-order80/src/main/java/com/sun/springcloud/OrderMain80.java
+++ cloud-consumer-order80/src/main/java/com/sun/springcloud/OrderMain80.java
@@ -1,11 +1,15 @@
 package com.sun.springcloud;
 
+import com.sun.myrule.MySelfRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 
 @SpringBootApplication
 @EnableEurekaClient
+//将ribbon的负载均衡机制改为指定和自定义的轮询算法
+@RibbonClient(name = "CLOUD-PAYMENT-SERVICE" ,configuration = MySelfRule.class)
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
--- cloud-consumer-order80/src/main/java/com/sun/springcloud/controller/OrderController.java
+++ cloud-consumer-order80/src/main/java/com/sun/springcloud/controller/OrderController.java
@@ -3,6 +3,7 @@ package com.sun.springcloud.controller;
 import com.sun.springcloud.entities.CommonResult;
 import com.sun.springcloud.entities.Payment;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,10 @@ public class OrderController {
     //public static final String PAYMENT_URL = "http://localhost:8001";
     public static final String PAYMENT_URL = "http://CLOUD-PAYMENT-SERVICE";
 
+    /**
+     * restTemplate中forentity是为了得到更详细的对象信息包括相应头和响应体
+     * 而forobject只返回对象的json串
+     */
     @Resource
     private RestTemplate restTemplate;
 
@@ -28,6 +33,16 @@ public class OrderController {
     @GetMapping("/consumer/payment/get/{id}")
     public CommonResult getPayment(@PathVariable("id") Long id){
         return restTemplate.getForObject(PAYMENT_URL+"/payment/get/"+id,CommonResult.class);
+    }
+
+    @GetMapping("/consumer/payment/getForEntity/{id}")
+    public CommonResult getPayment2(@PathVariable("id") Long id){
+        ResponseEntity<CommonResult> entity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);
+        if(entity.getStatusCode().is2xxSuccessful()){
+            return entity.getBody();
+        }else{
+            return new CommonResult(500,"查询失败");
+        }
     }
 
 }
